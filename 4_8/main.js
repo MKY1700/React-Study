78083f4b499f3c13ff8ff3af18671fd33287675b
baqnngg <bbqdnrmas@gmail>
2024-04-08 21:15:36 +0900

    4_8

---
+++ 4_8/main.js
@@ -0,0 +1,46 @@
+const Name = "guenyoung"
+const email = "a64051700@gmail.com"
+const hello = 'Hello ${myName}??'
+
+console.log(hello)
+
+const number = 10;
+const number2 = 10.55;
+
+const checked = true;
+const isShow = false;
+
+let undef;
+const obj = {abc:123};
+console.log(undef);
+console.log(obj.abc);
+console.log(obj.xyz);
+
+let name = null;
+console.log(name);
+name = "hihihi";
+console.log(name);
+
+const user = {
+    userName : "gy",
+    age : 18,
+    isVaild : true 
+};
+console.log(user.userName);
+console.log(user.age);
+console.log(user.isVaild);
+
+const fruits = ['Apple', 'Banana', 'Cherry'];
+console.log(fruits[0]);
+
+isShow = true;
+if (isShow) {
+    var aaa = "A";
+    console.log(aaa);
+}
+console.log(aaa);
+
+const a = 12;
+console.log(a);
+a = 999;
+console.log(a);
