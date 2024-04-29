38d16f4460adad513328a08f6695c10119063d15
MKY1700 <a64051700@gmail.com>
2024-04-29 21:11:25 +0900

    4_29

---
+++ 4_29/main.js
@@ -0,0 +1,70 @@
+// let time = prompt("시간을 입력하세요");
+
+// if (time < 12) {
+//     alert('오전입니다');
+// } else {
+//     alert('오후입니다');
+// }
+
+
+
+// let money = prompt("달러를 입력하세요");
+
+// won = money * 1300;
+// alert(won);
+
+
+
+// console.log(2 + 2 - 2 * 2 / 2 * 2)
+
+
+
+// let A = "aaa";
+// console.log(typeof(A));
+
+
+
+// let a = "가나다"+"라마바사";
+// console.log(a);
+// let num = 123+456;
+// console.log(num);
+
+
+
+// let score = prompt("점수를 입력하세요");
+
+// if(score == 4.5) {
+//     document.write("신");
+// } else if(score >= 4.0 && score <= 4.4) {
+//     document.write("교수님");
+// } else if(score >= 3.0 && score <= 3.9) {
+//     document.write("인간");
+// } else if(score >= 2.0 && score <= 2.9) {
+//     document.write("돌고래");
+// } else if(score >= 1.0 && score <= 1.9){
+//     document.write("플랑크톤");
+// }
+
+
+
+// let score = Number(prompt('학점을 입력하세요', '학점'));
+
+// if(score === 4.5) {
+//     alert('신');
+// } else if (4.0 <= score) {
+//     alert('교수님');
+// } else if (3.0 <= score) {
+//     alert('인간');
+// } else if (2.0 <= score) {
+//     alert('돌고래');
+// } else if (1.0 <= score) {
+//     alert('벌레련');
+// } else {
+//     alert('4.5보다 크거나 1.0보다 작음');
+// }
+
+
+
+for (let i = 0; i < 10; i++) {
+    console.log('${i}번 반복');
+}
