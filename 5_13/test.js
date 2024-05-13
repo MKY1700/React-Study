38d568e5c4ae1e8f0a9f9497ea4a28ce9238484e
MKY1700 <a64051700@gmail.com>
2024-05-13 21:03:11 +0900

    5_13

---
+++ 5_13/test.js
@@ -0,0 +1,19 @@
+// // {
+// //     let pi = 3.141592
+// //     console.log(`main.js의 pi는${pi}`);
+// // }
+
+
+
+// function sample() {
+//     let pi = 3.141592
+//     console.log(`main.js의 pi는${pi}`);
+// }
+// sample();
+
+
+
+(function () {
+    let pi = 3.141592
+    console.log(`main.js의 pi는${pi} 입니다.`);
+})()
