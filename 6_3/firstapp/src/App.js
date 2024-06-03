35afed7370d6c82715a9a59dca1eb13fca420c77
MKY1700 <a64051700@gmail.com>
2024-06-03 21:14:18 +0900

    6_3

--- 6_3/firstapp/src/App.js
+++ 6_3/firstapp/src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import MyComponent from './MyComponent.js'
+import MyComponent from './MyComponent.js';
 
 function App() {
-  return <MyComponent/>;
+  return <MyComponent number={3}>리액트</MyComponent>;
 }
 
-class App extends Component{ 
-  render() {
-    const name = 'react';
-    return <div className='react'>{name}</div>;
-  }
-}
+// class App extends Component{ 
+//   render() {
+//     const name = 'react';
+//     return <div className='react'>{name}</div>;
+//   }
+// }
 export default App;
--- 6_3/firstapp/src/MyComponent.js
+++ 6_3/firstapp/src/MyComponent.js
@@ -1,4 +1,32 @@
-const MyComponenet = () => {
-    return <div>새로운 컴포넌트</div>
+// const MyComponenet = props => {
+//     const {name, children} = props;
+//     return <div>안녕하세요 제 이름은 {props.name} 입니다. <br />
+//     children 값은 {props.children}
+//     입니다.
+//     </div>
+// }
+
+import { render } from "@testing-library/react";
+import { Component } from "react";
+import PropTypes from 'prop-types';
+
+class MyComponent extends Component {
+    render() {
+        const {name, number, children} = this.props;
+        return (
+            <div>안녕하세요 제 이름은 {name} 입니다. <br />
+            children 값은 {children} 입니다.
+            제가 좋아하는 숫자는 {number} 입니다.
+            </div>
+        );
+    }
 }
-export default MyComponenet
+MyComponent.defaultProps = {
+    name : "기본 이름"
+};
+//프롭타입으로 검사
+MyComponent.propTypes = {
+    name : PropTypes.string,
+    number : PropTypes.number.isRequired
+};
+export default MyComponent
