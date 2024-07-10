67ecb2dc71a994f5f34464396bb8a61764ae5146
a64051700@gmail.com <a64051700@gmail.com>
2024-07-10 19:51:11 +0900

    7_10

---
+++ shop/src/App.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import {Button, Navbar, Container, Nav} from 'react-bootstrap';
+import data from './data.js'
+import { useState } from 'react';
+
+function App() {
+  let [shoes] = useState
+
+  return (
+    <div className="App">
+      <Navbar bg="light" varinat="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      <div className="container">
+        <div className='row'>
+          <Card shoes={shoes[0]} i={1}></Card>
+          <Card shoes={shoes[1]} i={2}></Card>
+          <Card shoes={shoes[2]}>i={3}</Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+function Card(props) {
+  return (
+    <div className='row'>
+          <div className='col-md-4'>
+            <img src={process.env.PUBLIC_URL + '/shoe'+'props.i'+'.png'} width="80%"></img>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+        </div>
+  )
+}
+export default App;
