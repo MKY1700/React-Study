b2a184de67b727d04498c361856af6920acb7643
MKY1700 <a64051700@gmail.com>
2024-07-11 19:17:05 +0900

    shop

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,43 +1,53 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Button, Navbar, Container, Nav} from 'react-bootstrap';
-import data from './data.js'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import data from './Routes/data.js'
+import Detail from './Routes/Detail.js';
 import { useState } from 'react';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 function App() {
-  let [shoes] = useState
+  let [shoes] = useState(data)
+  let navigate = useNavigate();
 
   return (
-    <div className="App">
-      <Navbar bg="light" varinat="light">
+    <div className="Main">
+      <Navbar bg='light' variant='light'>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href='#home'>ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/')}}>HOME</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/detail')}}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg'></div>
-      <div className="container">
-        <div className='row'>
-          <Card shoes={shoes[0]} i={1}></Card>
-          <Card shoes={shoes[1]} i={2}></Card>
-          <Card shoes={shoes[2]}>i={3}</Card>
-        </div>
-      </div>
+
+      <Routes>
+        <Route path='/' element={
+          <>
+          <div className='main-bg'></div>
+          <div className='container'>
+            <div className='row'>
+              <Card shoes={shoes[0]} i={1}></Card>
+              <Card shoes={shoes[1]} i={2}></Card>
+              <Card shoes={shoes[2]} i={3}></Card>
+            </div>
+          </div>
+          </>
+        }></Route>
+        <Route path='/detail' element={<Detail/>}></Route>
+      </Routes>
     </div>
   );
 }
-function Card(props) {
+
+function Card(props){
   return (
-    <div className='row'>
-          <div className='col-md-4'>
-            <img src={process.env.PUBLIC_URL + '/shoe'+'props.i'+'.png'} width="80%"></img>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-        </div>
+    <div className='col-md-4'>
+        <img src={process.env.PUBLIC_URL + '/shoe'+props.i+'.png'} width="70%"></img>
+        <h4>{props.shoes.title}</h4>
+        <p>{props.shoes.price}</p>
+    </div>
   )
 }
 export default App;
